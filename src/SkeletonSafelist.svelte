322136ec83b05f1f98b976233e9b56e276cd9ec0
Kabira K. <kabira@monallabs.in>
2024-03-14 15:44:21 +0530

    safelist for skeleton ui variants

--- src/SkeletonSafelist.svelte
+++ src/SkeletonSafelist.svelte
@@ -2,4 +2,7 @@
 </script>
 
 
-<div class="alert variant-ghost alert-message alert-actions btn variant-filled"></div>
+<div class="badge variant-filled variant-filled-primary variant-filled-secondary variant-filled-tertiary variant-filled-success badge variant-filled-warning variant-filled-error variant-filled-surface variant-ghost variant-ghost-primary variant-ghost-secondary variant-ghost-tertiary variant-ghost-success variant-ghost-warning variant-ghost-error variant-ghost-surface variant-soft variant-soft-primary variant-soft-secondary variant-soft-tertiary variant-soft-success variant-soft-warning variant-soft-error variant-soft-surface variant-ringed variant-ringed-primary variant-ringed-secondary variant-ringed-tertiary variant-ringed-success variant-ringed-warning variant-ringed-error variant-ringed-surface variant-glass variant-glass-primary variant-glass-secondary variant-glass-tertiary variant-glass-success variant-glass-warning variant-glass-error variant-glass-surface
+"></div>
+
+<div class="fa-solid fa-skull"></div>
