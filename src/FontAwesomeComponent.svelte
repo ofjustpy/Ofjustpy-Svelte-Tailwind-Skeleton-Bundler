69eef9d0d872425e9067750e752e2dc3c593d27f
Kabira K. <kabira@monallabs.in>
2024-03-19 09:00:27 +0530

    Fontawesome and skeleton toast utility

---
+++ src/FontAwesomeComponent.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let jp_props;
+    import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+    import * as icons from '@fortawesome/free-solid-svg-icons';
+
+  const descriptionObject = {
+    ...jp_props.attrs,
+    style: jp_props.style,
+    class: jp_props.classes, 
+  };
+
+ console.log("IN FontAwesomeComponents with tag = ");
+ console.log(jp_props.html_tag);
+ console.log(jp_props.icon_label);
+ console.log(descriptionObject);
+ 
+</script>
+
+<FontAwesomeIcon icon={icons[jp_props.icon_label]} {...descriptionObject} />
+
---
+++ src/SkeletonUtilities.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { initializeStores } from '@skeletonlabs/skeleton';
+
+import type { ToastSettings, ToastStore } from '@skeletonlabs/skeleton';
+import { getToastStore } from '@skeletonlabs/skeleton';
+import { Toast} from '@skeletonlabs/skeleton';
+
+initializeStores();
+export let toastStore = getToastStore();
+
+export function triggerToast(term: string): void {
+		const t: ToastSettings = { message: `You selected the <u>${term}</u> action.` };
+		toastStore.trigger(t);
+	}
+
+
+</script>
+<Toast />
